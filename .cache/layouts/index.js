4476d92dfd357254c100490fa538cc878a02d17d
Tomislav Babic <tomislavb@gmail.com>
2018-10-05 23:33:53 +0200

    change file name

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "d:/git/cv/src/layouts/index.js"
-  import data from "d:\\git\\cv\\.cache\\json\\layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
