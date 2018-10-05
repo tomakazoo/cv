cfb6ea13cf80cc3ab777acb98d14712d15fc0c1f
Tomislav Babic <tomislavb@gmail.com>
2018-10-05 23:45:20 +0200

    change file name

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => (
     <div className="banner-text">
       <h1 className="responsive-headline">I'm Tomislav Babic.</h1>
       <h3>
-        I'm a passionate <span>software developer</span>,
+        I'm a passionate <span>software developer</span>, 
         <span>architect</span> and <span>innovative IT professional</span> offering great experience leveraging software
         engineering and DevOps methodologies to deliver highly effective and creative solutions to business and
         technology challenges. Let's
